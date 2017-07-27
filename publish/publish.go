3749d0f76c00f0586c14963884c3bad8f4190d89
Josh Curl <josh@curl.me>
2017-07-27 13:50:45 -0700

    Turn off event publishing for now

--- publish/publish.go
+++ publish/publish.go
@@ -2,18 +2,18 @@ package publish
 
 import (
 	"github.com/rancher/go-rancher/v2"
-	"github.com/rancherlabs/kattle/utils"
 
 	"k8s.io/client-go/pkg/api/v1"
 )
 
 func Pod(rancherClient *client.RancherClient, pod v1.Pod) error {
-	data := map[string]interface{}{}
+	return nil
+	/*data := map[string]interface{}{}
 	if err := utils.ConvertByJSON(pod, &data); err != nil {
 		return err
 	}
 	_, err := rancherClient.Publish.Create(&client.Publish{
 		Data: data,
 	})
-	return err
+	return err*/
 }

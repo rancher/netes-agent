094094999f9829ade448f50b5272fabd35199a4d
Josh Curl <josh@curl.me>
2017-07-27 13:58:58 -0700

    Update vendor

--- vendor/github.com/Sirupsen/logrus/terminal_notwindows.go
+++ vendor/github.com/Sirupsen/logrus/terminal_notwindows.go
@@ -3,19 +3,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build linux,!appengine darwin freebsd
+// +build linux darwin freebsd openbsd netbsd dragonfly nacl
+// +build !appengine
 
 package logrus
 
 import (
-	"syscall"
+	"io"
+	"os"
 	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
-// IsTerminal returns true if the given file descriptor is a terminal.
-func IsTerminal() bool {
-	fd := syscall.Stdout
+// IsTerminal returns true if stderr's file descriptor is a terminal.
+func IsTerminal(f io.Writer) bool {
 	var termios Termios
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
-	return err == 0
+	switch v := f.(type) {
+	case *os.File:
+		_, _, err := unix.Syscall6(unix.SYS_IOCTL, uintptr(v.Fd()), ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
+		return err == 0
+	default:
+		return false
+	}
 }

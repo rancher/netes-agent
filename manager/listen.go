e2ff49fe60e8482087ada7294aa5826921a2ff6f
Alena Prokharchyk <alena@rancher.com>
2017-09-19 13:26:11 -0700

    listen for deploymentunit.remove instead of instance.remove

--- manager/listen.go
+++ manager/listen.go
@@ -9,9 +9,9 @@ func (m *Manager) Listen() error {
 	logrus.Infof("Listening for events on %s", m.rancherClient.GetOpts().Url)
 
 	router, err := events.NewEventRouter(m.rancherClient, 250, map[string]events.EventHandler{
-		"instance.start":  wrapHandler(m.HandleComputeInstanceActivate),
-		"instance.remove": wrapHandler(m.HandleComputeInstanceRemove),
-		"cluster.remove":  wrapHandler(m.handleClusterRemove),
+		"instance.start":        wrapHandler(m.HandleComputeInstanceActivate),
+		"deploymentunit.remove": wrapHandler(m.HandleComputeInstanceRemove),
+		"cluster.remove":        wrapHandler(m.handleClusterRemove),
 	})
 	if err != nil {
 		return err
